443e72f7a4b8e35ce0e4a79f288e9bee93e99225
David Menache <dmenache@devopsandplatforms.com>
2023-08-17 16:19:02 -0600

    comment acutal product edit api calls

--- update-products-v3.py
+++ update-products-v3.py
@@ -1,19 +1,17 @@
 import openai
-import os
+import html
 import re
+import os
 import ssl
 import nltk
 import requests
-
 if not nltk.data.find('tokenizers/punkt'):
     nltk.download('punkt', quiet=True)
-
 credentials = {}
 creds_file_path = os.path.join(
           os.path.dirname(os.path.abspath(__file__)),
           "../creds.txt"
           )
-
 with open(creds_file_path) as f:
     current_section = None
     for line in f:
@@ -23,23 +21,20 @@ with open(creds_file_path) as f:
         elif current_section == "sanramon.doap.com":
             key, value = line.split(" = ")
             credentials[key] = value
-
-base_url = "https://sanramon.doap.com/wp-json/wc/v3/products"
-
+openai.api_key = credentials["openai.api_key"]
 auth = (
     credentials["sanramon.doap.com_consumer_key"],
     credentials["sanramon.doap.com_consumer_secret"]
     )
-
+base_url = "https://sanramon.doap.com/wp-json/wc/v3/products"
+counter = 0
 page = 1
 while True:
-    response = requests.get(f'{base_url}?page={page}', auth=auth)
+    response = requests.get(f'{base_url}?page={page}&per_page=10', auth=auth)  # Modify URL to include per_page=3
     response.raise_for_status()
     products = response.json()
-
     if not products:
         break
-
     for product in products:
         # Use OpenAI API to generate new product name
         response = openai.ChatCompletion.create(
@@ -51,17 +46,29 @@ while True:
         )
         
         new_product_name = response['choices'][0]['message']['content'].strip()
-        
-        product['name'] = new_product_name
-        print(f'Product ID: {product["id"]}, '
-              f'SKU: {product["sku"]}, '
-              f'New Name: {product["name"]}, '   # Modified product name will be displayed
-              f'Permalink: {product["permalink"]}')
-              
-        # Updating product name on the server
-        #update_url = f'{base_url}/{product["id"]}'
-        #update_response = requests.put(update_url, json=product, auth=auth)
-        #update_response.raise_for_status()
-        
+
+        replacements = ['<br>', '<br />', '<p>', '</p>', '<h5>', '</h5>', '\n', '"', "'"]
+        for rep in replacements:
+            product['name'] = product['name'].replace(rep, '')
+            product['short_description'] = html.unescape(product['short_description'].replace(rep, ''))
+            product['description'] = html.unescape(product['description'].replace(rep, ''))
+
+        # Adding ' - SALE' from product name
+        # product['name'] += ' - SALE'
+        counter = counter + 1
+        print(
+                f'ID: {product["id"]}  '
+                f'\nSku: {product["sku"]}  '
+                f'\nPermalink: {product["permalink"]}'
+                f'\nCurrent Name: {product["name"]}  '
+                f'\nCurrent Short Description: {product["short_description"]}  '
+                f'\nCurrent Description: {product["description"]}  '
+                f'\n'
+                )
+        # Uncomment the next three lines to update products. 
+        # update_url = f'{base_url}/{product["id"]}'
+        # update_response = requests.put(update_url, json=product, auth=auth)
+        # update_response.raise_for_status
     page += 1
-    break
+
+print("Product found: ", counter)
